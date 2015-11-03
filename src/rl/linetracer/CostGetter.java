1c1ea1dcbc61bee413b78ea4f2b3ef7633f975ef
verxirtam <d-kosuga@jcom.home.ne.jp>
2015-11-04 01:25:39 +0900

    cost算出用の基底クラスを作成した。

---
+++ src/rl/linetracer/CostGetter.java
@@ -0,0 +1,40 @@
+package rl.linetracer;
+
+import rl.Step;
+
+//costの算出を行うクラス
+//step("次"のステップ)の値を読み取ってcostを算出し、返り値としてcostを返却する。
+//状況に応じて4種のメソッドを利用する。
+//getCostWhenGoal()		:ゴール時のcostを算出
+//getCostWhenCourseOut():コースアウト時のcostを算出(定義済み)
+//getCostWhenTimeOut()	:タイムアウト時のcostを算出(定義済み)
+//getCostWhenRunning()	:実行時のcostを算出
+public abstract class CostGetter
+{
+	//costの最大値
+	private double costMax;
+
+	public CostGetter(double cost_max)
+	{
+		costMax = cost_max;
+	}
+
+	//ゴール時のcostを算出
+	abstract public double getCostWhenGoal(Step step, double elapsed_time);
+
+	//コースアウト時のcostを算出(定義済み)
+	public double getCostWhenCourseOut(Step step)
+	{
+		return costMax;
+	}
+	
+	//タイムアウト時のcostを算出(定義済み)
+	public double getCostWhenTimeOut(Step step, double elapsed_time)
+	{
+		return costMax;
+	}
+
+	//実行時のcostを算出
+	abstract public double getCostWhenRunning(Step step, double elapsed_time);
+
+}
